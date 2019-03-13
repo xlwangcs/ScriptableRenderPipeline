e3bc1b08ac493345e62d6429be43f30b12b5e936
Anis Benyoub <anis@unity3d.com>
2019-03-13 02:04:25 +0100

    Displaying the first shadow in the raytraced area shadow debug menu Adding a history analytic buffer to invalidate samples for the temporal filtering of area shadows Correcting an exposure behavior in the primary ray

--- com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/Camera/HDCameraFrameHistoryType.cs
+++ com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/Camera/HDCameraFrameHistoryType.cs
@@ -12,6 +12,7 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
 #if ENABLE_RAYTRACING
         RaytracedAmbientOcclusion,
         RaytracedAreaShadow,
+        RaytracedAreaAnalytic,
         RaytracedReflection,
         RaytracedIndirectDiffuse,
 #endif
